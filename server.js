922ccbfeb08eb8d09cd09708305e5976dad152f6
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2024-01-08 05:04:59 -0500

    Cors Policy Added

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const express = require("express");
 const router = require("./router/auth-router");
 const connectDB = require("./utils/db");
 const ContactRouter = require("./router/contact-router");
+const cors = require("cors");
 
 const app = express();
-
+// To Tackle Cors Error
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 // Middleware
 
 app.use(express.json());
@@ -14,7 +21,7 @@ app.use(express.json());
 app.use("/api/auth", router);
 app.use("/api/form", ContactRouter);
 
-const PORT = 5000;
+const PORT = 3000;
 
 connectDB().then(() => {
   app.listen(PORT, () => {
